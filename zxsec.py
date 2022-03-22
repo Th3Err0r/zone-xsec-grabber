bd763a6d407554ef79f55c0d666de99a01cb6722
Th3 Err0r <70305545+Th3Err0r@users.noreply.github.com>
2022-03-22 17:24:00 +0530

    Add files via upload

---
+++ zxsec.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+from hashlib import new
+import requests
+import re
+from datetime import datetime
+import time
+
+t1=datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")
+
+url = str(input("URL => "))
+pnum = int(input("Pages => "))
+try:
+	for a in range(1,pnum+1):
+		r = requests.get(url+str(a)).text
+		r = r.replace("\r\n", "")
+		print('\n',url,a,'\n')		
+		#print(r)
+		preg = re.findall("            <td></td>            <td>(.*?)</td>            <td><a", r)
+		preg = [preg for preg in preg if preg != '']
+		#print(preg)
+		for line in preg:
+			#print(line)
+			line = line.split('</td>            <td></td>            <td>')
+			line = (line[-1])
+			line = line.split('/')
+			line = line[0]
+			if "<a" in line or "href=" in line or "<img" in line:
+				pass
+			else:
+				print('http://'+line)
+				with open('zxrez/'+t1+'.txt', 'a') as rrr:
+					rrr.write('http://'+line+'\n')
+except Exception as e :
+	print(e)
